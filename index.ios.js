8b5baed43669e259bfed39d8f0b9f6a9e207dfa1
Todd Briley <todd.briley@outlook.com>
2017-05-19 13:09:51 -0400

    step 7

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-export default class albums extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+//Import a library to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+// Create a component (a component is a JS function that returns some amount of JSX)
+const App = () => (
+  
+    <View>
+        <Header headerText={'Albums!'} />
+        <AlbumList />
+    </View>
+  );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('albums', () => albums);
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumList.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data }));
+
+    axios.post('http://et.briley.org/api/expenses', {
+          token: 'd033549a-b58c-4625-9d78-61d1b8212145',
+          timeFrame: 'prior30days'
+        })
+    .then(response => console.log('expenses: ', response.data))
+    .catch(err => console.log(err.message));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />);
+  }
+
+  render() {
+      console.log(this.state);
+      return (
+        <View>
+            {this.renderAlbums()}
+        </View>
+      );
+  }
+}
+
+export default AlbumList;
